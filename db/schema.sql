b64a065a129db822112b99be539c3410f857cd93
Heather Violet <heather@heathers-air.lan>
2022-02-05 15:00:00 -0800

    added questions

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,20 @@ CREATE TABLE department (
     id INTERGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INTERGER AUTO_INCREMENT PRIMARY,
+    job_title VARCHAR(30) NOT NULL,
+    department_role VARCHAR(30) NOT NULL,
+    salary INTERGER(12) NOT NULL
+);
+
+CREATE TABLE employee (
+    id INTERGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INTERGER(12) NOT NULL,
+    reporting_manager VARCHAR(30)
+);
--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,51 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// creares an array of questions
-const questions = [
+// prompts questions
+const promptQuestions = () => {
+    inquirer.prompt ([
     {
         type: 'choices',
         name: 'list',
         message: 'What would you like to do?',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update and employee role']
-    },
+        choices: [
+            'View all departments', 
+            'View all roles',
+            'View all employees', 
+            'Add a department', 
+            'Add a role', 
+            'Add an employee',
+            'Update an employee role']
+    }
+]).then(function (answer) {
+    switch (answer.promptQuestions) {
+        case 'View all departments':
+            viewAllDepartments();
+        break;
     
+        case 'View all roles':
+            viewAllRoles();
+        break;
 
-]
+        case 'View all employees':
+            viewAllEmployees();
+        break;
+
+        case 'Add a department':
+            addADepartment();
+        break;
+
+        case 'Add a role':
+            addARole();
+        break;
+
+        case 'Add an Employee':
+            addAnEmployee();
+        break;
+
+        case 'Update an employee role':
+            updateAnEmployeeRole();
+        break;
+    }
+    })
+}
